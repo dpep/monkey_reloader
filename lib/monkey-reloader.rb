ad4f0142a9ee8993465a714fe5fed5c0c518876e
dpepper <pepper.daniel@gmail.com>
2016-12-13 13:51:17 -0800

    add branch detection

--- lib/monkey-reloader.rb
+++ lib/monkey-reloader.rb
@@ -1,10 +1,48 @@
 module MonkeyReloader
-  def self.load(blacklist = [])
-    cmd = "git status -s | grep '.rb' | awk '{ print $2 }'"
-    `#{cmd}`.split.reject do |file|
-      blacklist.include? file
-    end.each do |file|
-      Kernel.load file unless blacklist.include? file
+  class << self
+
+
+    def init(blacklist = [])
+      @@blacklist = Array(blacklist)
+      update_hash
+
+      true
     end
+
+    def load
+      files = changed_files.reject do |file|
+        @@blacklist.include? file
+      end
+      update_hash
+
+      files.each do |file|
+        Kernel.load file
+      end
+    end
+
+
+    private
+
+    def update_hash
+      @@hash = `git rev-parse HEAD`.strip
+    end
+
+    def changed_files
+      files = []
+      awk = "awk '{ print $2 }'"
+
+      # find recent changes
+      files += `git status -s | #{awk}`.split
+
+      # find committed changes if there was a branch change / rebase
+      files += `git diff --name-status HEAD..#{@@hash} | #{awk}`.split
+
+      # filter for ruby files and those that dissapeared during
+      # a branch change
+      files.select do |file|
+        /\.rb$/.match file and File.exists? file
+      end
+    end
+
   end
 end
