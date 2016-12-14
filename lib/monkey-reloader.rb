14f9c202f5953f62dd35b0698370f048eb0ac1a3
dpepper <pepper.daniel@gmail.com>
2016-12-14 14:32:58 -0800

    add whitelist/blacklist

--- lib/monkey-reloader.rb
+++ lib/monkey-reloader.rb
@@ -1,46 +1,113 @@
+require 'set'
+require 'pathname'
+
+
 module MonkeyReloader
   class << self
 
+    @@hash = 'HEAD'
+    @@whitelist = nil
+    @@blacklist = nil
 
-    def init(blacklist = [])
-      @@blacklist = Array(blacklist)
+
+    def init(whitelist = [], blacklist = [])
+      @@whitelist = Set.new
+      self.whitelist whitelist
+
+      @@blacklist = Set.new
+      self.blacklist blacklist
+
       update_hash
 
-      true
+      self
     end
 
     def load
-      files = changed_files.reject do |file|
-        @@blacklist.include? file
+      wlist = whitelist
+      blist = blacklist
+
+      files = changed_files.select do |file|
+        wlist.include? file
+      end.reject do |file|
+        blist.include? file
       end
+
       update_hash
+      pwd = Pathname.new Dir.pwd
 
       files.each do |file|
         Kernel.load file
+      end.map do |file|
+        # map back to relative pathnames for convenience
+        Pathname.new(file).relative_path_from(pwd).to_s
       end
+    end
+
+    def whitelist(files = [])
+      @@whitelist ||= Set.new
+
+      expand_paths @@whitelist.merge parse_paths files
+    end
+
+    def blacklist(files = [])
+      @@blacklist ||= Set.new
+
+      expand_paths @@blacklist.merge parse_paths files
     end
 
 
     private
 
+    def parse_paths(paths = [])
+      Array(paths).map do |path|
+        path = File.expand_path path
+
+        if path.include? '*'
+          path
+        elsif Dir.exists? path
+          "#{path}/**/*.rb"
+        elsif File.exists? path
+          unless /\.rb$/.match path
+            raise ArgumentError.new ".rb files only: #{path}"
+          end
+
+          path
+        else
+          raise ArgumentError.new "path not found: #{path}"
+        end
+      end
+    end
+
+    def expand_paths(paths = [])
+      # recalculate path expansions - caching will miss filesystem changes
+      Array(paths).map do |path|
+        Dir[path]
+      end.flatten
+    end
+
     def update_hash
+      # get current git branch hash
       @@hash = `git rev-parse HEAD`.strip
     end
 
     def changed_files
-      files = []
+      # return a list of changed files
+
+      files = Set.new
       awk = "awk '{ print $2 }'"
 
       # find recent changes
-      files += `git status -s | #{awk}`.split
+      files.merge `git status -s | #{awk}`.split
 
       # find committed changes if there was a branch change / rebase
-      files += `git diff --name-status HEAD..#{@@hash} | #{awk}`.split
+      files.merge `git diff --name-status HEAD..#{@@hash} | #{awk}`.split
 
       # filter for ruby files and those that dissapeared during
       # a branch change
       files.select do |file|
         /\.rb$/.match file and File.exists? file
+      end.map do |file|
+        File.expand_path file
       end
     end
 
